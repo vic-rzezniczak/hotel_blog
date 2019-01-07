f04fcf6c44ca7281f128529e2c9a530fdf873684
Wiktor Rzeźniczak <vic.rzezniczak@gmail.com>
2019-01-07 21:35:55 +0100

    adding simple views and urls

--- hotel_blog/urls.py
+++ hotel_blog/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = [
-    path('', views.index, name='index'),]
+urlpatterns = [    
+    path('', views.index, name='index'), # ex: /hotel_blog/
+    path('<int:question_id>/', views.detail, name='detail'), # ex: /hotel_blog/5/
+    path('<int:question_id>/results/', views.results, name='results'), # ex: /hotel_blog/5/results/
+    path('<int:question_id>/vote/', views.vote, name='vote'), # ex: /hotel_blog/5/vote/
+    ]
--- hotel_blog/views.py
+++ hotel_blog/views.py
@@ -5,3 +5,16 @@ import os
 
 def index(request):
     return HttpResponse(f'Hello, world. You\'re at the {os.getcwd()} index.')
+
+
+def detail(request, question_id):
+    return HttpResponse(f'You\'re looking at question {question_id}.')
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse(f'You\'re voting on question {question_id}.')
