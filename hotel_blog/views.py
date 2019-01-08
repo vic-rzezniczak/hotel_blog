1f0566ad885e547f73be7594a3b0a0cc5834cfd9
Wiktor Rzeźniczak <vic.rzezniczak@gmail.com>
2019-01-08 19:32:22 +0100

    first template

--- hotel_blog/views.py
+++ hotel_blog/views.py
@@ -1,10 +1,14 @@
 from django.http import HttpResponse
-import os
+from django.template import loader
+from django.shortcuts import render
 
+from .models import Question
 
 
 def index(request):
-    return HttpResponse(f'Hello, world. You\'re at the {os.getcwd()} index.')
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'hotel_blog/index.html', context)
 
 
 def detail(request, question_id):
